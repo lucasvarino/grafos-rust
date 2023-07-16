b706b3ff9b7700ec90b3ae0973a3f9c33556f054
nicolassoam <nsm4456@gmail.com>
2023-07-16 03:07:49 -0300

    prints de teste na main

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,10 @@ fn main() {
 
     let weight:i32 = graph_result.get_edge_weight(1, 3).unwrap();
     println!("Peso da aresta entre 1 e 3: {}", weight);
+
+    let neighbors = graph_result.get_closed_neighborhood(1);
+    println!("Vizinhos fechados do no 1: {:?}", neighbors);
+
+    let neighbors = graph_result.get_open_neighborhood(1);
+    println!("Vizinhos abertos do no 1: {:?}", neighbors);
 }
