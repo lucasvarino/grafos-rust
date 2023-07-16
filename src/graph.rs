dd1144be6c9f4885189fd0c673bddd470b6a717b
Lucas Varino <lucasvarino@gmail.com>
2023-07-15 21:22:46 -0300

    Funcionalidades Iniciais

---
+++ src/graph.rs
@@ -0,0 +1,81 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::path::Path;
+
+#[derive(Debug)]
+pub struct Graph {
+    adj_list: HashMap<usize, HashSet<usize>>,
+}
+
+impl Graph {
+    pub fn new() -> Self {
+        Graph {
+            adj_list: HashMap::new(),
+        }
+    }
+
+    pub fn add_edge(&mut self, src: usize, dest: usize) {
+        self.adj_list
+            .entry(src)
+            .or_insert_with(HashSet::new)
+            .insert(dest);
+
+        self.adj_list
+            .entry(dest)
+            .or_insert_with(HashSet::new)
+            .insert(src);
+    }
+
+    fn remove_all_edges(&mut self, node: i32) {
+        for (_node, edges) in self.adj_list.iter_mut() {
+            edges.remove(&(node as usize));
+        }
+    }
+
+    pub fn remove_node(&mut self, node: i32) {
+        match self.adj_list.remove(&(node as usize)) {
+            Some(_) => self.remove_all_edges(node),
+            None => (),
+        }
+    }
+
+    pub fn print_graph(&self) {
+        for (node, edges) in &self.adj_list {
+            println!("Adjacency list of node {}:", node);
+            print!("head");
+
+            for e in edges {
+                print!(" -> {}", e);
+            }
+
+            println!();
+        }
+    }
+}
+
+pub fn read_graph_from_file<P>(filename: P) -> Result<Graph, io::Error>
+where
+    P: AsRef<Path>,
+{
+    let mut graph = Graph::new();
+
+    let file = File::open(filename)?;
+    let reader = io::BufReader::new(file);
+
+    for (index, line) in reader.lines().enumerate() {
+        let line = line?;
+        let parts: Vec<&str> = line.split_whitespace().collect();
+
+        if index > 0 && parts[0] == "e" {
+            let src: usize = parts[1].parse().unwrap();
+            let dst: usize = parts[2].parse().unwrap();
+
+            graph.add_edge(src, dst);
+        }
+    }
+
+    Ok(graph)
+}
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod graph;
+
+fn main() {
+    let mut graph_result = graph::read_graph_from_file("test.txt").unwrap_or_else(|e| {
+        eprintln!("Erro ao ler o arquivo de grafo: {}", e);
+        std::process::exit(1);
+    });
+
+    graph_result.print_graph();
+
+    graph_result.remove_node(1);
+
+    println!("Remoção do no 1");
+    graph_result.print_graph();
+}
