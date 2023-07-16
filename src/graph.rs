c1e5855eeb6bda901d10f7ff5e96c75d05ba5f3e
nicolassoam <nsm4456@gmail.com>
2023-07-16 00:18:29 -0300

    tentando inserir peso nas arestas

--- src/graph.rs
+++ src/graph.rs
@@ -3,12 +3,55 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
+use std::borrow::Borrow;
+use std::hash::Hash;
 use std::path::Path;
+
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+pub struct Edge {
+    dest: usize,
+    weight: i32,
+}
+// impl PartialEq for Edge {
+//     fn eq(&self, other: &Edge) -> bool {
+//         self.dest == other.dest
+//     }
+// }
+
+// impl Hash for Edge {
+//     fn hash<H: Hasher>(&self, state: &mut H) {
+//         self.dest.hash(state);
+//     }
+// }
+
+impl Borrow<usize> for Edge {
+    fn borrow(&self) -> &usize {
+        &self.dest
+    }
+}
+
+impl Edge {
+
+    pub fn new(dest: usize, weight: i32) -> Self {
+        Edge { dest, weight }
+    }
+    
+    pub fn get_dest(&self) -> usize {
+        self.dest
+    }
+
+    pub fn get_weight(&self) -> i32 {
+        self.weight
+    }
+
+}
+
 
 #[derive(Debug)]
 pub struct Graph {
-    adj_list: HashMap<usize, HashSet<usize>>,
+    adj_list: HashMap<usize, HashSet<Edge>>,
 }
+
 
 impl Graph {
     pub fn new() -> Self {
@@ -17,21 +60,33 @@ impl Graph {
         }
     }
 
-    pub fn add_edge(&mut self, src: usize, dest: usize) {
+    pub fn add_edge(&mut self, src: usize, dest: usize, weight: i32) {
+        if weight < 1 {
+            panic!("Peso da aresta deve ser maior que 0");
+        }
+
         self.adj_list
             .entry(src)
             .or_insert_with(HashSet::new)
-            .insert(dest);
+            .insert(Edge { dest, weight });
 
         self.adj_list
             .entry(dest)
             .or_insert_with(HashSet::new)
-            .insert(src);
+            .insert(Edge { dest:src, weight });
+    }
+
+    pub fn get_edge_weight(&mut self, src:i32, dest:i32) -> io::Result<i32>{
+        let src_usize = src as usize;
+        let dest_usize = dest as usize;
+        let weight: i32 = self.adj_list.entry(src_usize).or_default().get(&dest_usize).unwrap().get_weight();
+        Ok(weight)
     }
 
     pub fn remove_edge(&mut self, src: i32, dest: i32) -> io::Result<()> {
         let src_usize = src as usize;
         let dest_usize = dest as usize;
+        // let weight: i32 = self.get_edge_weight(src, dest)?;
 
         let remove_src = self
             .adj_list
@@ -74,7 +129,7 @@ impl Graph {
             print!("head");
 
             for e in edges {
-                print!(" -> {}", e);
+                print!(" -> {}", e.dest);
             }
 
             println!();
@@ -98,8 +153,9 @@ where
         if index > 0 && parts[0] == "e" {
             let src: usize = parts[1].parse().unwrap();
             let dst: usize = parts[2].parse().unwrap();
-
-            graph.add_edge(src, dst);
+            // let weight: i32 = parts[3].parse().unwrap();
+            let weight: i32 = 1;
+            graph.add_edge(src, dst, weight);
         }
     }
 
