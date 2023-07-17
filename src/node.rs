5cab6570db2b079625d9603295973d988cb86649
Lucas Varino <lucasvarino@gmail.com>
2023-07-17 10:38:06 -0300

    Adicionando getters

--- src/node.rs
+++ src/node.rs
@@ -8,4 +8,12 @@ impl Node {
     pub fn new(id: i32, weight: f32) -> Self {
         Node { id, weight }
     }
+
+    pub fn get_weight(&self) -> f32 {
+        self.weight
+    }
+
+    pub fn get_id(&self) -> i32 {
+        self.id
+    }
 }
